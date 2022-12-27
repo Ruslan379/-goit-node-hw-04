a127234cd563eed05f16cc4f50bbda2c81875d3b
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-27 19:29:50 +0200

    Work - M4-1_20

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,7 @@ const loginController = async (req, res) => {
     const { email, password } = req.body;
     const token = await login(email, password);
 
-    res.json({ status: "success" }, token);
+    res.json({ status: "success", token });
 };
 
 
