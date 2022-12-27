a8867f2c8ae846b0c36eceeffce60f7370046c53
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-27 17:52:39 +0200

    Work - M4-1_10

--- services/authService.js
+++ services/authService.js
@@ -1,12 +1,14 @@
 const { User } = require("../models/userModel.js");
-
+const bcrypt = require("bcrypt")
 
 //-----------------------------------------------------------------------------
-const registration = async () => {
-
+const registration = async (req, res) => {
+    const { email, password } = req.body;
+    const user = new User({ email, password: bcrypt.hash(password, 10) });
+    await user.save();
 };
 
-const login = async () => {
+const login = async (req, res) => {
 
 };
 
