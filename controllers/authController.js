acaa0e063e56df4e474d4e9b09da1f898395e834
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-27 18:01:27 +0200

    Work - M4-1_13

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,11 @@
 const { registration, login } = require("../services/authService.js")
 
 
-const registrationController = (req, res) => {
+const registrationController = async (req, res) => {
     const { email, password } = req.body;
+    await registration(email, password);
+
+    res.json({ status: "success" });
 };
 
 
