5bb68dbca54aa3f0380cd9fed246c01650aa1ccc
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-27 18:08:01 +0200

    Work - M4-1_15

--- services/authService.js
+++ services/authService.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt")
 const registration = async (email, password) => {
     const user = new User({
         email,
-        password: bcrypt.hash(password, 10)
+        password: await bcrypt.hash(password, 10)
     });
     await user.save();
 };
