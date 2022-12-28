2f5c037f5994efa5083a560b52d8608375d4fbcf
Ruslan379 <arnov@i.ua>
2022-12-28 13:28:28 +0200

    Work - M4-1_36

--- controllers/contacts/updatePatchContactFavorite.js
+++ controllers/contacts/updatePatchContactFavorite.js
@@ -24,11 +24,42 @@ const updatePatchContactFavorite = async (req, res, next) => {
         throw new BadRequest("missing field favorite")
     }
 
-    const contact = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true });
+    // const contact = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true });
+
+    const { _id: user_id } = req.user //?
+    //* =============================console===================================
+    console.log("updatePatchContactFavorite-->req.user:".bgYellow.red); //?
+    console.table(req.user); //?
+    console.table([req.user]);
+
+    console.log("updatePatchContactFavorite-->user_id:".bgYellow.blue, user_id); //?
+    console.log("");
+    //* =======================================================================
+
+    //! ===========================console============================
+    console.log("START-->PATCH/:id/favorite".rainbow); //!
+    lineBreak();
+    //! ==============================================================
+
+    const contact = await Contact.findOneAndUpdate({ _id: contactId, userId: user_id }, { favorite }, { new: true });
+
 
     if (!contact) {
+        //! ===========================console============================
+        console.log("Нет ПОЛЬЗОВАТЕЛЯ с таким ID:".yellow, contactId.red); //!
+        lineBreak();
+        console.log("END-->PATCH/:id/favorite".rainbow); //!
+        //! ==============================================================
         throw new NotFound(`Contact wiht id:'${contactId}' not found`)
     }
+
+    //! ===========================console============================
+    console.log(`ОБНОВЛЕННЫЙ ПОЛЬЗОВАТЕЛЬ с ID: ${contactId}:`.rainbow); //!
+    console.log(contact); //!
+    lineBreak();
+    console.log("END-->PATCH/:id/favorite".rainbow); //!
+    lineBreak();
+    //! ==============================================================
 
     res.status(200).json({
         status: "success",
