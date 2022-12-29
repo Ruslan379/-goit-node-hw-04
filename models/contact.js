0013102a70d9f95e044fc569e2847174d5fe7c5d
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-29 14:16:45 +0200

    Work --> M4-3_46

--- models/contact.js
+++ models/contact.js
@@ -1,4 +1,5 @@
 //? +++++++++++++++++++  mongoose +++++++++++++++++++
+const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
@@ -25,7 +26,8 @@ const contactSchema = Schema({
         type: String,
     },
     userId: {
-        type: String,
+        // type: String,
+        type: mongoose.Schema.Types.ObjectId,
         // required: true,
     },
     favorite: {
