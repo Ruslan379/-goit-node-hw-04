1cc9b14e9a7ea0ba5e23948497b7e6a8a4f3ecc3
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-30 20:56:38 +0200

    Work - M4_доп.мат-1_57

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ const authRouter = require('./routes/api/authRouter.js') //?
 //---------------------------routes-------------------------------
 //! auth
 
-//! POST --> http://localhost:3001/api/users/signup
-//! POST --> http://localhost:3001/api/users/login
+// POST --> http://localhost:3001/api/users/signup
+// POST --> http://localhost:3001/api/users/login
 
-//  POST --> http://localhost:3001/api/auth/registration
-//  POST --> http://localhost:3001/api/auth/login
+//!  POST --> http://localhost:3001/api/auth/registration
+//!  POST --> http://localhost:3001/api/auth/login
 
 //  Headers --> Authorization -->
 //  Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2FiNGQzOTFiMmMxNDJjOGY2OTM1YjIiLCJlbWFpbCI6IjU1NUB1a3IubmV0IiwiY3JlYXRlZEF0IjoiMjAyMi0xMi0yN1QxOTo1MzoyOS42MjJaIiwiaWF0IjoxNjcyMzM4ODk2fQ.OF7nTx66ljHbC90VfIGsXGxwLK3ulHIrF104g55g7bA
